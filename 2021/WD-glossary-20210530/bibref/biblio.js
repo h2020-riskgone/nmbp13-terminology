23dc3c5758ee5df86366a0161b0e42f5c2cb8abd
Egon Willighagen <egon.willighagen@gmail.com>
2022-03-29 14:42:52 +0200

    'Archived' the released version

---
+++ 2021/WD-glossary-20210530/bibref/biblio.js
@@ -0,0 +1,3 @@
+berjon.biblio = {
+   "Duffus2017" : "Duffus, J., Templeton, D.M., Schwenk, M., 2017, Comprehensive Glossary of Terms Used in Toxicology, <a href=\"https://doi.org/10.1039/9781782623724\">10.1039/9781782623724</a>."
+}
